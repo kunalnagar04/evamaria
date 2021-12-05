59979a0433a6fc05b11844169ac586f04e8e5248
Movieshouse8 <95081598+Movieshouse8@users.noreply.github.com>
2021-12-05 07:48:15 +0530

    Script.py

--- Script.py
+++ Script.py
@@ -1,22 +1,30 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍
+    START_TXT = """<b>Hᴇʟʟᴏ {},
 
-𝚖𝚊𝚒𝚗𝚝𝚎𝚗𝚊𝚗𝚌𝚎 𝚋𝚢​ @Movieshouse8 💕"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/kunal_nagar_01>Kunal Nagar</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v2.0"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- I am a close source project. 
-- <b>Mᴀɪɴᴛᴇɴᴀɴᴄᴇ Bʏ​ @Movieshouse8 💕</b>
-<b>DEVS:</b>
-- <a href=https://t.me/kunal_nagar_01>kunal Nagar</a>"""
+Mʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>, I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇs Aɴᴅ Sᴇʀɪᴇs, Jᴜsᴛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Eɴᴊᴏʏ 😍
+
+Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ: @Movieshouse8 🍷</b>"""
+    HELP_TXT = """<b>Hᴇʏ {}
+
+Nᴏᴛᴇ 🎓
+• Iᴛ's Nᴏᴛ Cᴏᴍᴘʟɪᴄᴀᴛᴇᴅ....🤓
+• Dᴏɴ'ᴛ Sᴘᴀᴍ Mᴇ...🤒
+• Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs...😍
+
+Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ: @Movieshouse8 🍷</b>"""
+    ABOUT_TXT = """<b>✯ Mʏ Nᴀᴍᴇ: {}
+
+✯ Cʀᴇᴀᴛᴏʀ: href=https://t.me/kunal_nagar_01>Kunal࿐</a>
+
+✯ Mʏ Gʀᴏᴜᴘ: href=https://t.me/MH_request>Mʜ ʀᴇǫᴜᴇsT</a>
+
+✯ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: ᴠ2.0 [Sᴛᴀʙʟᴇ]</b>"""
+    SOURCE_TXT = """<b>Nᴏᴛᴇ 💨
+• Tʜɪs Is A Oᴘᴇɴ Sᴏᴜʀᴄᴇ Bᴏᴛ.
+
+• Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ: href=https://t.me/MH_request>Mʜ ʀᴇǫᴜᴇsT</a>
+
+• Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ: href=https://t.me/Movieshouse8>Movies House 8</a> 🍷</b>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and Movieshouse8 Bot will respond whenever a keyword is found the message
